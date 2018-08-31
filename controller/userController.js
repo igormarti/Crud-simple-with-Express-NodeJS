6276df059a24df9d5679f399dcc97d1ea885ed3e
Igor Martins <igorm573@gmail.com>
2018-08-31 11:43:30 -0300

    Comments added

--- controller/userController.js
+++ controller/userController.js
@@ -1,34 +1,31 @@
+    var user = require('../models/Users');//Importing the model of users.
+    var users;//Declaring variable that receive the users.
 
-    //Importing the model of users.
-    var user = require('../models/Users');
-    //Declaring variable that storage the users.
-    var users;
-    //Method to get list of users and render in a view.
-    exports.index = function(req,res){
-        users = user.list();
-        res.render('user/index',{users});
+    exports.index = function(req,res){//Method to get list of users and render in a view.
+        users = user.list(); //Receiving all the users.
+        res.render('user/index',{users});//Redering a view with all the users.
     };
-    //Method to render the create view.
-    exports.create = function(req,res){
-        res.render('user/add');
+    
+    exports.create = function(req,res){//Method to renders the create view.
+        res.render('user/add');//Renders the view that create one user.
     };
-    //Method to render the edit view.
-    exports.edit = function(req,res){
-        let u = user.get(req.params.name);
-        res.render('user/edit',{u});
+   
+    exports.edit = function(req,res){ //Method to renders the edit view.
+        let u = user.get(req.params.name);//Get the user to edit.
+        res.render('user/edit',{u});//Renders the view with the user to be edited.
     };
-    //Method to save user, after render in index view.
-    exports.add = function(req,res){
-        user.save(req,res);
-        res.redirect('/users');
+    
+    exports.add = function(req,res){//Method to save user, after render in index view.
+        user.save(req,res);//Store user.
+        res.redirect('/users');//Redirecting to users view.
     };
-    //Method to edit user, after render in index view.
-    exports.alter = function(req,res){
-        user.update(req,res);
-        res.redirect('/users');
+    
+    exports.alter = function(req,res){//Method to edit user, after render in index view.
+        user.update(req,res);//Update user.
+        res.redirect('/users');//Redirecting to users view.
     };
-    //Method to delete a user and render index view.
-    exports.delete = function(req,res){
-        user.remove(req,res);
-        res.redirect('/users');
+    
+    exports.delete = function(req,res){//Method to delete a user and render index view.
+        user.remove(req,res);//Remove user.
+        res.redirect('/users');//Redirecting to users view.
     };
--- models/Users.js
+++ models/Users.js
@@ -1,42 +1,39 @@
 
     //Here could have any connection with an data base.
     //--------------------------------------------------
-
-    //This is a array, where storage the data.
-    var users = ['Igor','Juliana'];
-    //This method is resposable by storage data in array.
-    exports.save = function(req,res){
-        users.push(req.body.name);
-        sort(users);
-        return users;
-    };
-    //This method update the data of array.
-    exports.update = function(req,res){
-        var i = users.indexOf(req.body.nameold);
-        users[i] = req.body.name;
-        sort(users);
-        return users;
-    };
-    //This method get a user specific of array.
-    exports.get = function(name){
-        let i = users.indexOf(name); 
-        return users[i];
-    };
-    //This method get all the data of array.
-    exports.list = function(){
-        sort(users);
-        return users;
-    };
-    //This method remove one user of array.
-    exports.remove = function(req,res){
-        let i = users.indexOf(req.params.name.toString());
-        users.splice(i,1);
-        sort(users);
-        return users;
+    var users = ['Igor','Juliana']; //This is a array, where storage the data.
+    exports.save = function(req,res){//This method is resposable by storage data in array.
+        users.push(req.body.name);//Storing user in users array.
+        sort(users);//Sorting users.
+        return users;//returning the users after of sorted.
     };
 
-    var sort = function(users){
-        users.sort();
+    exports.update = function(req,res){//This method update the data of array.
+        var i = users.indexOf(req.body.nameold);//Getting index of the user. 
+        users[i] = req.body.name;//Modifying the old name of the user by new name.
+        sort(users);//Sorting users.
+        return users;//Returning the users, after of sorted.
+    };
+    
+    exports.get = function(name){//This method get a user specific of array.
+        let i = users.indexOf(name); //Getting the index of the user by the name parameter.
+        return users[i];//Returning the user that references the index getted.
+    };
+    
+    exports.list = function(){//This method get all the data of array.
+        sort(users);//Sorting the users.
+        return users;//Returning the users, after of sorted.
+    };
+    
+    exports.remove = function(req,res){//This method remove one user of array.
+        let i = users.indexOf(req.params.name.toString());//Getting the index of the user by name parameter.
+        users.splice(i,1);//Removing user that references the index getted.
+        sort(users);//Sorting the users.
+        return users;//Returning users, after of sorted.
+    };
+    
+    var sort = function(users){//This method sort the data.
+        users.sort();//Sorting the users.
     }
 
     
